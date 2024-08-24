0f72d3a495fb1d4b80db0d8c1593074e1e2979d9
Nikhil241103 <pekacoclover@gmail.com>
2024-08-24 10:24:38 +0530

    configured routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import MainLayout from './components/layout/MainLayout'
+
+import { publicRoutes, privateRoutes } from "./routes/Routes"
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        {/* Public Routes */}
+        {
+          publicRoutes.map((route) => (
+            <Route path={route.path} element={route.element} />
+          ))
+        }
+        {/* Public Routes */}
+
+        {/* Private Routes */}
+        <Route path='/' element={<MainLayout />} >
+          {
+            privateRoutes.map((route, index) => (
+              route.index ? (
+                <Route index key={index} element={route.element} />
+              ) : (
+                <Route path={route.path} key={index} element={route.element} />
+              )
+            )) 
+          }
+        </Route>
+        {/* Private Routes */}
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
