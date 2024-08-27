14d90ea8643ad22d22e84c24823a0587791c93d5
Nikhil241103 <pekacoclover@gmail.com>
2024-08-27 13:17:55 +0530

    added styles and redux store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+
 import App from './App.jsx'
-import './index.css'
+
+import './css/index.css'
+import './css/utility.css'
+
+import store from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
