a55ce9709b73d66ba7d1bf52baf4a572c78bc436
Nikhil241103 <pekacoclover@gmail.com>
2024-08-27 13:16:48 +0530

    added sign in page

--- client/src/pages/SigninPage.jsx
+++ client/src/pages/SigninPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
+import Welcome from '../components/common/Welcome'
+import SigninForm from '../components/common/SigninForm'
+
 const SigninPage = () => {
   return (
-    <div>SigninPage</div>
+    <main>
+      <section className='signin'>
+        <Welcome />
+        <SigninForm />
+      </section>
+    </main>
   )
 }
 
