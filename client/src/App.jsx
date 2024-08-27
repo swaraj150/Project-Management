ada203dbb415167b6ae8bc59bb900b8fd84e577f
Nikhil241103 <pekacoclover@gmail.com>
2024-08-27 16:00:55 +0530

    added toast container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,54 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 import MainLayout from './components/layout/MainLayout'
 
-import { publicRoutes, privateRoutes } from "./routes/Routes"
+import { publicRoutes, privateRoutes } from './routes/Routes'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Public Routes */}
-        {
-          publicRoutes.map((route, index) => (
-            <Route path={route.path} key={index} element={route.element} />
-          ))
-        }
-        {/* Public Routes */}
-
-        {/* Private Routes */}
-        <Route path='/' element={<MainLayout />} >
+    <>
+      <ToastContainer
+        position='bottom-left'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='light'
+        transition:Bounce
+      />
+      <BrowserRouter>
+        <Routes>
+          {/* Public Routes */}
           {
-            privateRoutes.map((route, index) => (
-              route.index ? (
-                <Route index key={index} element={route.element} />
-              ) : (
-                <Route path={route.path} key={index} element={route.element} />
-              )
-            )) 
+            publicRoutes.map((route, index) => (
+              <Route path={route.path} key={index} element={route.element} />
+            ))
           }
-        </Route>
-        {/* Private Routes */}
+          {/* Public Routes */}
 
-      </Routes>
-    </BrowserRouter>
+          {/* Private Routes */}
+          <Route path='/' element={<MainLayout />} >
+            {
+              privateRoutes.map((route, index) => (
+                route.index ? (
+                  <Route index key={index} element={route.element} />
+                ) : (
+                  <Route path={route.path} key={index} element={route.element} />
+                )
+              ))
+            }
+          </Route>
+          {/* Private Routes */}
+
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
