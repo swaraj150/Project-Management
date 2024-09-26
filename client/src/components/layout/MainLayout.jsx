9bb3857234537ba20058c0358ee97a5877c8475d
Nikhil241103 <pekacoclover@gmail.com>
2024-09-26 20:31:10 +0530

    added logic for auto login

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Outlet, useNavigate } from 'react-router-dom'
+
+import userApi from '../../api/modules/user.api'
+
+import { setUser } from '../../redux/features/userSlice'
 
 const MainLayout = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const { user } = useSelector((state) => state.user)
+
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      const { res, err } = await userApi.getInfo()
+
+      if (res) {
+        dispatch(setUser(res))
+        toast.success('Login successful. Welcome back!')
+      }
+
+      if (err) navigate('/sign-in')
+    }
+
+    if (user === null) fetchUserDetails()
+  }, [])
+
   return (
     <>
       <main>
