3dc40aa13724b0f392427cffe3c7793fdb3b4e9d
Nikhil241103 <pekacoclover@gmail.com>
2024-09-27 16:04:58 +0530

    disabled strict mode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,11 +13,13 @@ import './css/utility.css'
 import store from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_OAUTH2_GOOGLE_CLIENT_ID}>
       <Provider store={store}>
         <App />
       </Provider>
     </GoogleOAuthProvider>
-  </StrictMode>,
+  // </StrictMode>
 )
+
+// Disabled strict mode to avoid rendering twice, as code required to get access token for github login can be used only once
