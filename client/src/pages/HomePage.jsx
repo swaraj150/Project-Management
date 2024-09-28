2aeefb386cfdfb1f678b8f720833e5cab568d346
Nikhil241103 <pekacoclover@gmail.com>
2024-09-28 15:12:45 +0530

    added all components according to menu

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import MainMenu from '../components/common/MainMenu'
+import DashBoard from '../components/common/DashBoard'
+import Organization from '../components/common/Organization'
+import Projects from '../components/common/Projects'
+import Tasks from '../components/common/Tasks'
+import Alerts from '../components/common/Alerts'
 
 const HomePage = () => {
+  const [active, setActive] = useState(0)
+
+  const menuItems = [
+    <DashBoard />,
+    <Organization />,
+    <Projects />,
+    <Tasks />,
+    <Alerts />
+  ]
+
   return (
-    <div>HomePage</div>
+    <section id="homepage">
+      <MainMenu active={active} setActive={setActive} />
+      <section className="content">
+        {menuItems[active]}
+      </section>
+    </section>
   )
 }
 
