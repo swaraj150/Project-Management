682673e0faf9de5a97b44a58e9457644f06a714c
Nikhil241103 <pekacoclover@gmail.com>
2024-09-29 19:26:59 +0530

    added logic for auto login based on access token

--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 import Welcome from '../components/common/Welcome'
 import SignupForm from '../components/common/SignupForm'
 
 const SignupPage = () => {
+  const navigate = useNavigate()
+
+  const { user } = useSelector((state) => state.user)
+
+  useEffect(() => {
+    if (user) navigate('/')
+    
+    const token = localStorage.getItem('token')
+
+    if (token !== null) navigate('/')
+  }, [])
+
   return (
     <section className='signin'>
       <Welcome />
