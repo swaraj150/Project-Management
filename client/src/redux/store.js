6160e1842c13beab7c1f60441a0be8fa1853156a
Nikhil241103 <pekacoclover@gmail.com>
2024-09-29 19:25:32 +0530

    added menu slice

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import userSlice from './features/userSlice'
 import organizationSlice from './features/organizationSlice'
+import menuSlice from './features/menuSlice'
 
 const store = configureStore({
   reducer: {
     user: userSlice,
-    organization: organizationSlice
+    organization: organizationSlice,
+    menu: menuSlice
   }
 })
 
