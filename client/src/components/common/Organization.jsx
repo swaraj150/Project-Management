eca431de035c67130263d70b200bc10ba21682cf
Nikhil241103 <pekacoclover@gmail.com>
2024-09-29 19:29:06 +0530

    added organization components

--- client/src/components/common/Organization.jsx
+++ client/src/components/common/Organization.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
+import OrganizationHero from './OrganizationHero'
+import OrganizationInfo from './OrganizationInfo'
+import OrganizationHome from './OrganizationHome'
 
 const Organization = () => {
+  const { organization } = useSelector((state) => state.organization)
+
   return (
-    <section className="organization">
-      
+    <section id="organization">
+      {
+        organization ? (
+          <OrganizationHome />
+        ) : (
+          <>
+            <OrganizationHero />
+            <OrganizationInfo />
+          </>
+        )
+      }
     </section>
   )
 }
