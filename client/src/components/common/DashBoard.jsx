68acfa0230a844b13843835f0f405879bf5493f0
Nikhil241103 <pekacoclover@gmail.com>
2024-09-29 19:28:46 +0530

    added greeting

--- client/src/components/common/DashBoard.jsx
+++ client/src/components/common/DashBoard.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const DashBoard = () => {
+  const { user } = useSelector((state) => state.user)
+
+  const getGreeting = () => {
+    const currentHour = new Date().getHours()
+
+    if (currentHour >= 5 && currentHour < 12) {
+      return (
+        <span>Good Morning</span>
+      )
+    } else if (currentHour >= 12 && currentHour < 17) {
+      return (
+        <span>Good Afternoon</span>
+      )
+    } else if (currentHour >= 17 && currentHour < 21) {
+      return (
+        <span>Good Evening</span>
+      )
+    } else {
+      return (
+        <span>Welcome</span>
+      )
+    }
+  }
+
   return (
-    <div>DashBoard</div>
+    <section id="dashboard">
+      <h1>
+        {getGreeting()}, <span>{user.username}</span>
+      </h1>
+    </section>
   )
 }
 
