371bcd9db23168c2b73c050e028d178694e2f545
Nikhil241103 <pekacoclover@gmail.com>
2024-10-02 20:43:22 +0530

    added logic to fetch organization after user login

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Outlet, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
+import organizationApi from '../../api/modules/organization.api'
 import userApi from '../../api/modules/user.api'
 
+import { setOrganization } from '../../redux/features/organizationSlice'
 import { setUser } from '../../redux/features/userSlice'
 
 const MainLayout = () => {
@@ -13,24 +16,34 @@ const MainLayout = () => {
   const { user } = useSelector((state) => state.user)
 
   useEffect(() => {
+    const fetchOrganization = async () => {
+      const { res, err } = await organizationApi.getInfo()
+
+      if (res) dispatch(setOrganization(res))
+    }
+
     const fetchUserDetails = async () => {
       const { res, err } = await userApi.getInfo()
 
       if (res) {
         dispatch(setUser(res))
         toast.success('Login successful. Welcome back!')
+        fetchOrganization()
+      } else {
+        localStorage.removeItem('token')
+        navigate('/sign-in')
       }
-
-      if (err) navigate('/sign-in')
     }
 
-    if (user === null) fetchUserDetails()
+    const token = localStorage.getItem('token')
+
+    if (token && user === null) fetchUserDetails()
   }, [])
 
   return (
     <>
       <main className='no-scrollbar'>
-        <Outlet />
+        { user && <Outlet /> }
       </main>
     </>
   )
