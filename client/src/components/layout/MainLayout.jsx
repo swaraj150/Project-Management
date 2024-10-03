2addff27139d8c1e2fce754f5c5beb4a19a31b02
Nikhil241103 <pekacoclover@gmail.com>
2024-10-03 16:32:26 +0530

    added llogic to fetch organization and projects

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -4,9 +4,13 @@ import { Outlet, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 
 import organizationApi from '../../api/modules/organization.api'
+import projectsApi from '../../api/modules/projects.api'
+import teamsApi from '../../api/modules/teams.api'
 import userApi from '../../api/modules/user.api'
 
 import { setOrganization } from '../../redux/features/organizationSlice'
+import { setProjects } from '../../redux/features/projectsSlice'
+import { setTeams } from '../../redux/features/teamsSlice'
 import { setUser } from '../../redux/features/userSlice'
 
 const MainLayout = () => {
@@ -16,19 +20,12 @@ const MainLayout = () => {
   const { user } = useSelector((state) => state.user)
 
   useEffect(() => {
-    const fetchOrganization = async () => {
-      const { res, err } = await organizationApi.getInfo()
-
-      if (res) dispatch(setOrganization(res))
-    }
-
-    const fetchUserDetails = async () => {
+      const fetchUserDetails = async () => {
       const { res, err } = await userApi.getInfo()
 
       if (res) {
         dispatch(setUser(res))
         toast.success('Login successful. Welcome back!')
-        fetchOrganization()
       } else {
         localStorage.removeItem('token')
         navigate('/sign-in')
@@ -37,8 +34,35 @@ const MainLayout = () => {
 
     const token = localStorage.getItem('token')
 
-    if (token && user === null) fetchUserDetails()
+    if (user == null && token === null) navigate('/sign-in')
+    if (user === null) fetchUserDetails()
   }, [])
+
+  useEffect(() => {
+    const fetchOrganization = async () => {
+      const { res, err } = await organizationApi.getInfo()
+
+      if (res) dispatch(setOrganization(res))
+    }
+
+    const fetchProjects = async () => {
+      const { res, err } = await projectsApi.getAll()
+
+      if (res) dispatch(setProjects(res))
+    }
+
+    const fetchTeams = async () => {
+      const { res, err } = await teamsApi.getAll()
+
+      if (res) dispatch(setTeams(res))
+    }
+
+    if (user) {
+      fetchOrganization()
+      fetchProjects()
+      fetchTeams()
+    }
+  }, [user])
 
   return (
     <>
