aae20dbafc9b7fc244d40c047b89697637d5b90a
Nikhil241103 <pekacoclover@gmail.com>
2024-10-03 16:33:28 +0530

    added project and teams slice

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,12 +3,16 @@ import { configureStore } from '@reduxjs/toolkit'
 import userSlice from './features/userSlice'
 import organizationSlice from './features/organizationSlice'
 import menuSlice from './features/menuSlice'
+import projectsSlice from './features/projectsSlice'
+import teamsSlice from './features/teamsSlice'
 
 const store = configureStore({
   reducer: {
     user: userSlice,
     organization: organizationSlice,
-    menu: menuSlice
+    menu: menuSlice,
+    projects: projectsSlice,
+    teams: teamsSlice
   }
 })
 
