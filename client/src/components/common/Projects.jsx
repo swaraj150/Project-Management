77f4773608faf89f191a607f2ae040c42225a382
Nikhil241103 <pekacoclover@gmail.com>
2024-10-03 16:31:43 +0530

    added project components

--- client/src/components/common/Projects.jsx
+++ client/src/components/common/Projects.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
+import ProjectsHome from './ProjectsHome'
+import ProjectsInfo from './ProjectsInfo'
 
 const Projects = () => {
+  const { organization } = useSelector((state) => state.organization)
+
   return (
-    <div>Projects</div>
+    <section id="projects">
+      {
+        organization ? (
+          <ProjectsHome />
+        ) : (
+          <ProjectsInfo />
+        )
+      }
+    </section>
   )
 }
 
