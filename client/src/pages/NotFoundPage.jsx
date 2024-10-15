90e1efd7317cb59f0d2c982ee0232fae6124df28
Nikhil241103 <pekacoclover@gmail.com>
2024-10-15 12:23:46 +0530

    added not found page

---
+++ client/src/pages/NotFoundPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const NotFoundPage = () => {
+  const navigate = useNavigate()
+
+  return (
+    <div className="not-found paper">
+      <h1>404 - Page Not Found</h1>
+      <p>Oops! The page you are looking for doesn't exist.</p>
+      <div className="cta">
+        <div className="cta-btn ptr" onClick={() => navigate(-1)}>Go Back</div>
+        <div className="cta-btn ptr" onClick={() => navigate('/')}>Go Home</div>
+      </div>
+    </div>
+  )
+}
+
+export default NotFoundPage
