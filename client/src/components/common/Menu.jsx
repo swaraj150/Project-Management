df0bd950fe3849d9a533a6e1ac2f9a9ed2c5d88e
Nikhil241103 <pekacoclover@gmail.com>
2024-10-16 18:59:56 +0530

    modified menu styling

---
+++ client/src/components/common/Menu.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { MdDashboard, MdGroups, MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from "react-icons/md"
+import { FaBuilding, FaProjectDiagram, FaTasks } from "react-icons/fa"
+
+import Logo from '../../assets/logo.png'
+
+import { setActive, setCollapsed } from '../../redux/features/menuSlice'
+
+const menuItems = [
+  {
+    name: 'DashBoard',
+    icon: <MdDashboard />
+  },
+  {
+    name: 'Organization',
+    icon: <FaBuilding />
+  },
+  {
+    name: 'Teams',
+    icon: <MdGroups />
+  },
+  {
+    name: 'Projects',
+    icon: <FaProjectDiagram />
+  },
+  {
+    name: 'Tasks',
+    icon: <FaTasks />
+  }
+]
+
+const Menu = () => {
+  const dispatch = useDispatch()
+
+  const { collapsed } = useSelector((state) => state.menu)
+
+  const [selected, setSelected] = useState(0)
+
+  const handleChange = (index) => {
+    setSelected(index)
+    dispatch(setActive(index))
+  }
+
+  return (
+    <section id="menu" className={collapsed ? "collapsed" : null} >
+      <div className="collapse-btn pointer" onClick={() => dispatch(setCollapsed(!collapsed))}>
+        {
+          collapsed ? <MdOutlineKeyboardArrowRight /> : <MdOutlineKeyboardArrowLeft />
+        }
+      </div>
+      <div className="menu-title">
+        <img src={Logo} alt="logo" />
+        {
+          collapsed ? null : <p>Project Maestro</p>
+        }
+      </div>
+      <ul className="menu-items">
+        {
+          menuItems.map((item, index) => (
+            <li key={index} className={selected === index ? "active pointer paper" : "pointer" } onClick={() => handleChange(index)}>
+              {item.icon} {collapsed ? null : item.name}
+            </li>
+          ))
+        }
+      </ul>
+      <ul className="menu-user-settings">
+
+      </ul>
+    </section>
+  )
+}
+
+export default Menu
