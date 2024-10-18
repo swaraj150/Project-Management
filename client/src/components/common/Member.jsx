d1d2571e987f730433455f1fe261db872e7c4096
Nikhil241103 <pekacoclover@gmail.com>
2024-10-18 17:49:04 +0530

    added component for member info

---
+++ client/src/components/common/Member.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { MdEdit } from 'react-icons/md'
+import { BsPersonFillX } from 'react-icons/bs'
+
+import { isFixedRole, roleFormatter } from '../../utils/roles.util'
+import RoleChange from './RoleChange'
+
+const Member = ({ member, isAuthorized }) => {
+  const modalRef = useRef(null)
+
+  const [modalOpen, setModalOpen] = useState(false)
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) setModalOpen(false)
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  })
+
+  return (
+    <li>
+      {modalOpen ? <RoleChange member={member} setModalOpen={setModalOpen} modalRef={modalRef} /> : null}
+      <div className="member-info">
+        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" />
+        <p>{member.name} &nbsp;&nbsp;
+          <a href={`mailto:${member.emails[0]}`} className="opacity-5" >
+            {member.emails[0]}
+          </a>
+        </p>
+      </div>
+      <div className={`cta ${isAuthorized ? 'authorized' : ''}`}>
+        <div className="role" style={isAuthorized ? { justifyContent: "center" } : null}>
+          <p className="opacity-5" >{roleFormatter(member.projectRole)}</p>
+          {
+            isFixedRole(member.projectRole)
+              ? null
+              : isAuthorized ? <MdEdit className="opacity-5 pointer" onClick={() => setModalOpen(!modalOpen)} /> : null
+          }
+        </div>
+        {isAuthorized ? <BsPersonFillX className="opacity-5 pointer" /> : null}
+      </div>
+    </li>
+  )
+}
+
+export default Member
