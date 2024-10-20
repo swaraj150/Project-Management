a577d5559bf09b300170f48aa6cb644d128a7e09
Nikhil241103 <pekacoclover@gmail.com>
2024-10-20 18:59:56 +0530

    added collapsed state

--- client/src/redux/features/menuSlice.js
+++ client/src/redux/features/menuSlice.js
@@ -3,17 +3,22 @@ import { createSlice } from '@reduxjs/toolkit'
 export const menuSlice = createSlice({
   name: 'Menu',
   initialState: {
-    active: 0
+    active: 0,
+    collapsed: false
   },
   reducers: {
     setActive: (state, action) => {
       state.active = action.payload
+    },
+    setCollapsed: (state, action) => {
+      state.collapsed = action.payload
     }
   }
 })
 
 export const {
   setActive,
+  setCollapsed
 } = menuSlice.actions
 
 export default menuSlice.reducer
