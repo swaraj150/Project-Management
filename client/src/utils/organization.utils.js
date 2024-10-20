995344e3d648e2ea623c0a94e2d6ea8c4ca811e2
Nikhil241103 <pekacoclover@gmail.com>
2024-10-20 09:50:50 +0530

    modified authorized user logic

--- client/src/utils/organization.utils.js
+++ client/src/utils/organization.utils.js
@@ -14,11 +14,6 @@ export const assignableRoles = [
   roles.qa
 ]
 
-export const isFixedRole = (role) => {
-  return role === roles.productOwner || role === roles.stakeholder
-}
-
 export const authorizedUser = (user) => {
-  console.log(user.projectRole, roles.productOwner)
-  return user.projectRole === roles.projectManager || user.projectRole === roles.productOwner
+  return user.projectRole === roles.productOwner
 }
