52466b0c66599c2578d586208475e1e5dd054d9b
Nikhil241103 <pekacoclover@gmail.com>
2024-10-20 09:48:50 +0530

    modified organization page

--- client/src/pages/Organization.jsx
+++ client/src/pages/Organization.jsx
@@ -1,26 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 import MembersList from '../components/common/MembersList'
+import RequestList from '../components/common/RequestList'
+
+import { authorizedUser } from '../utils/organization.utils'
 
 const Organization = () => {
+  const { user } = useSelector((state) => state.user)
   const { organization } = useSelector((state) => state.organization)
+
+  const [activeSection, setActiveSection] = useState(0)
+  const [isAuthorized, setIsAuthorized] = useState(false)
+
+  useEffect(() => {
+    setIsAuthorized(authorizedUser(user))
+  }, [user])
 
   return (
     <section id="organization">
-      <h2 className="title h1">{organization.name}</h2>
-      <div className="members">
-        <small className='opacity-5'>Manage members access</small>
-        <div className="members-info">
-          <div className="heading">
-            <h2 className="title">Members</h2>
-          </div>
-          <div className="cta">
-            <p>{organization.members.length} members</p>
-          </div>
-        </div>
-        <MembersList members={organization.members} />
+      <div className="organization-heading">
+        <h2 className="title h1">{organization.name}</h2>
+        {
+          isAuthorized ? (
+            <div className="cta">
+              <button
+                className={`pointer ${activeSection === 0 ? "active" : null}`}
+                onClick={() => setActiveSection(0)}
+              >
+                Members
+              </button>
+              <button
+                className={`pointer ${activeSection === 1 ? "active" : null}`}
+                onClick={() => setActiveSection(1)}
+              >
+                Requests
+              </button>
+            </div>
+          ) : null
+        }
       </div>
+      {
+        activeSection === 0
+          ? <MembersList members={organization.members} isAuthorized={isAuthorized} />
+          : <RequestList />
+      }
     </section>
   )
 }
