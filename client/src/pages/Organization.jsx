6d3e03531a657e807906bb76d7d327b2a414231b
Nikhil241103 <pekacoclover@gmail.com>
2024-10-20 18:56:57 +0530

    removed redundant isAuthorized prop and defined permissions according to roles

--- client/src/pages/Organization.jsx
+++ client/src/pages/Organization.jsx
@@ -1,28 +1,23 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 
 import MembersList from '../components/common/MembersList'
 import RequestList from '../components/common/RequestList'
 
-import { authorizedUser } from '../utils/organization.utils'
+import { roles } from '../utils/organization.utils'
 
 const Organization = () => {
   const { user } = useSelector((state) => state.user)
   const { organization } = useSelector((state) => state.organization)
 
   const [activeSection, setActiveSection] = useState(0)
-  const [isAuthorized, setIsAuthorized] = useState(false)
-
-  useEffect(() => {
-    setIsAuthorized(authorizedUser(user))
-  }, [user])
 
   return (
     <section id="organization">
       <div className="organization-heading">
         <h2 className="title h1">{organization.name}</h2>
         {
-          isAuthorized ? (
+          user.projectRole === roles.productOwner ? (
             <div className="cta">
               <button
                 className={`pointer ${activeSection === 0 ? "active" : null}`}
@@ -42,7 +37,7 @@ const Organization = () => {
       </div>
       {
         activeSection === 0
-          ? <MembersList members={organization.members} isAuthorized={isAuthorized} />
+          ? <MembersList members={organization.members} />
           : <RequestList />
       }
     </section>
