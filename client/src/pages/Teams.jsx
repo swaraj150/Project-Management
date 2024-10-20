0116b0d9f28ca4116c7ce2a65cc5cff222d28969
Nikhil241103 <pekacoclover@gmail.com>
2024-10-20 09:50:29 +0530

    modified team page

--- client/src/pages/Teams.jsx
+++ client/src/pages/Teams.jsx
@@ -1,25 +1,44 @@
-import React from 'react'
-import { MdDelete } from 'react-icons/md'
+import React, { useEffect, useRef, useState } from 'react'
+import { useSelector } from 'react-redux'
 import { IoMdAdd } from 'react-icons/io'
 
+import CreateTeam from '../components/common/CreateTeam'
 import TeamsList from '../components/common/TeamsList'
 
 const Teams = () => {
+  const modalRef = useRef(null)
+
+  const { teams } = useSelector((state) => state.teams)
+
+  const [modalOpen, setModalOpen] = useState(false)
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      const isSelectElement = event.target.parentElement?.classList.contains('css-tj5bde-Svg')
+
+      if (modalRef.current && !modalRef.current.contains(event.target) && !isSelectElement) {
+        setModalOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  })
+
   return (
     <section id="teams">
+      {modalOpen ? <CreateTeam setModalOpen={setModalOpen} modalRef={modalRef} /> : null}
       <div className="heading">
         <h2 className="title h1">Teams</h2>
-        <div className="cta">
-          <button>
-            <MdDelete />
-            <p>Delete Team</p>
-          </button>
-          <button>
-            <IoMdAdd />
-            <p>Create Team</p>
-          </button>
-        </div>
+        <button className="cta pointer" onClick={() => setModalOpen(true)}>
+          <IoMdAdd />
+          <p>Create Team</p>
+        </button>
       </div>
+      <p className="opacity-7">Teams Count: {teams.length}</p>
       <TeamsList />
     </section>
   )
