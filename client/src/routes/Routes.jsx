85824b584b789ebf171077fe6beea8366dc0fade
Nikhil241103 <pekacoclover@gmail.com>
2024-10-21 09:39:57 +0530

    added discover page

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import LandingPage from '../pages/LandingPage'
 import SigninPage from '../pages/SigninPage'
 import SignupPage from '../pages/SignupPage'
 import HomePage from '../pages/HomePage'
+import Discover from '../pages/Discover'
 
 export const publicRoutes = [
   {
@@ -22,5 +23,9 @@ export const privateRoutes = [
   {
     index: true,
     element: <HomePage />
+  },
+  {
+    path: 'discover',
+    element: <Discover />
   }
 ]
