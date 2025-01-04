130d6ff20a1250c2ff8e0d159511b188d1d847ce
swaraj150 <swaraj.andhale@vit.edu.in>
2025-01-04 15:24:34 +0530

    assignee list, project metrics (untested), milestone development in progress

--- server/src/main/java/com/example/server/repositories/IssueRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.server.repositories;
-
-import com.example.server.entities.Issue;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface IssueRepository extends JpaRepository<Issue, UUID> {
-}
