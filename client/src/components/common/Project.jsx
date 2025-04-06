c05242e48fb2c534ce96713fd5bc7fe8a6ba2f0c
Nikhil241103 <pekacoclover@gmail.com>
2025-04-06 16:13:10 +0530

    removed modal and used map from redux state

--- client/src/components/common/Project.jsx
+++ client/src/components/common/Project.jsx
@@ -1,38 +1,17 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import { FaRegCalendarAlt, FaRupeeSign } from 'react-icons/fa'
-
-import ProjectDetails from './ProjectDetails'
 
 import { formatDate, formatBudget } from '../../utils/project.utils'
 
 const Project = ({ project }) => {
-  const modalRef = useRef(null)
+  const navigate = useNavigate()
 
-  const [modalOpen, setModalOpen] = useState(false)
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      const isIndicatorClick = 
-        event.target.classList.contains('css-15lsz6c-indicatorContainer') ||
-        event.target.classList.contains('css-tj5bde-Svg') ||
-        event.target.tagName.toLowerCase() === 'path' ||
-        event.target.closest('.css-15lsz6c-indicatorContainer') !== null
-
-      if (modalRef.current && !modalRef.current.contains(event.target) && !isIndicatorClick) {
-        setModalOpen(false)
-      }
-    }
-
-    document.addEventListener('mousedown', handleClickOutside)
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-  })
+  const { membersMap } = useSelector((state) => state.organization)
 
   return (
-    <li className="project-info paper">
-      {modalOpen ? <ProjectDetails project={project} setModalOpen={setModalOpen} modalRef={modalRef} /> : null}
+    <li className="project-info paper-1">
       <h3>{project.title}</h3>
       <small className="opacity-5 description" >{project.description}</small>
       <div className="date opacity-7">
@@ -44,15 +23,15 @@ const Project = ({ project }) => {
         {formatBudget(project.budget)}
       </div>
       <div className="project-manager">
-        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" />
+        <img className='profile-img' src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" />
         <div className="project-manager-details">
-          <p>{project.projectManager.name} &nbsp;&nbsp;</p>
-          <a href={`mailto:${project.projectManager.emails[0]}`} className="opacity-5" >
-            {project.projectManager.emails[0]}
+          <p>{membersMap[project.projectManager].name} &nbsp;&nbsp;</p>
+          <a href={`mailto:${membersMap[project.projectManager].emails[0]}`} className="opacity-5" >
+            {membersMap[project.projectManager].emails[0]}
           </a>
         </div>
       </div>
-      <small className="opacity-7 pointer" onClick={() => setModalOpen(true)} >See project details and progress</small>
+      <small className="opacity-7 pointer" onClick={() => navigate(`/projects/${project.title}`, { state: { project } })} >See project details and progress</small>
     </li>
   )
 }
