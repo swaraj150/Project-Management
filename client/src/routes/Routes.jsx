9ab4e062a5a636476041cd9c0ab7340875795757
Nikhil241103 <pekacoclover@gmail.com>
2025-04-06 16:07:55 +0530

    added routes for seperated pages

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -5,8 +5,13 @@ import Discover from '../pages/Discover'
 import Dashboard from '../pages/DashBoard'
 import Organization from '../pages/Organization'
 import Teams from '../pages/Teams'
+import CreateTeam from '../pages/CreateTeam'
+import TeamDetails from '../pages/TeamDetails'
 import Projects from '../pages/Projects'
+import CreateProject from '../pages/CreateProject'
+import ProjectDetails from '../pages/ProjectDetails'
 import Tasks from '../pages/Tasks'
+import CreateTask from '../pages/CreateTask'
 
 export const publicRoutes = [
   {
@@ -41,11 +46,31 @@ export const privateRoutes = [
     element: <Teams />
   },
   {
+    path: 'teams/create',
+    element: <CreateTeam />
+  },
+  {
+    path: 'teams/:teamName',
+    element: <TeamDetails />
+  },
+  {
     path: 'projects',
     element: <Projects />
   },
   {
+    path: 'projects/create',
+    element: <CreateProject />
+  },
+  {
+    path: 'projects/:projectName',
+    element: <ProjectDetails />
+  },
+  {
     path: 'tasks',
     element: <Tasks />
+  },
+  {
+    path: 'tasks/create',
+    element: <CreateTask />
   }
 ]
