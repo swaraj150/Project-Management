0ed32d5b1749281b0d034f5b11e213cd7bb0ea4e
Nikhil241103 <pekacoclover@gmail.com>
2025-04-06 16:17:40 +0530

    removed modals and used map from redux state

--- client/src/components/common/Team.jsx
+++ client/src/components/common/Team.jsx
@@ -1,36 +1,23 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import { IoEllipsisHorizontalCircle } from 'react-icons/io5'
-
-import TeamDetails from './TeamDetails'
 
 import { membersCount, developersCount, testersCount } from '../../utils/team.utils'
 
 const Team = ({ team }) => {
-  const modalRef = useRef(null)
+  const navigate = useNavigate()
 
-  const [modalOpen, setModalOpen] = useState(false)
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (modalRef.current && !modalRef.current.contains(event.target)) setModalOpen(false)
-    }
-
-    document.addEventListener('mousedown', handleClickOutside)
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-  })
+  const { membersMap } = useSelector((state) => state.organization)
 
   return (
-    <div className="team-list-info">
-      {modalOpen ? <TeamDetails team={team} setModalOpen={setModalOpen} modalRef={modalRef} /> : null}
-      <p className="team-list-info-item" >{team.name}</p>
-      <p className="team-list-info-item" >{team.teamLead.name}</p>
-      <p className="team-list-info-item" >{membersCount(team)}</p>
-      <p className="team-list-info-item" >{developersCount(team)}</p>
-      <p className="team-list-info-item" >{testersCount(team)}</p>
-      <IoEllipsisHorizontalCircle className="pointer" onClick={() => setModalOpen(true)} />
+    <div className="teams-list-info paper-1">
+      <p className="teams-list-info-item" >{team.name}</p>
+      <p className="teams-list-info-item" >{membersMap[team.teamLead].name}</p>
+      <p className="teams-list-info-item" >{membersCount(team)}</p>
+      <p className="teams-list-info-item" >{developersCount(team)}</p>
+      <p className="teams-list-info-item" >{testersCount(team)}</p>
+      <IoEllipsisHorizontalCircle className="pointer" onClick={() => navigate(`/teams/${team.name}`, { state: { team } })} />
     </div>
   )
 }
