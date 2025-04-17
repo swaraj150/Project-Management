4502d07224ce5af009356b549bddeb44a727f18d
Nikhil241103 <pekacoclover@gmail.com>
2025-04-17 11:28:28 +0530

    added logic for sending message

--- client/src/components/common/ChatBoard.jsx
+++ client/src/components/common/ChatBoard.jsx
@@ -1,9 +1,59 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { useSelector } from 'react-redux'
 
-const ChatBoard = ({ chats }) => {
+import Message from './Message'
+
+import { useSocket } from '../../contexts/SocketContext'
+
+const ChatBoard = ({ id }) => {
+  const scrollRef = useRef(null)
+
+  const { chats } = useSelector((state) => state.chats)
+
+  const { sendMessageInChat } = useSocket()
+
+  const [message, setMessage] = useState('')
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault()
+      handleAddMessage()
+    }
+  }
+
+  const handleAddMessage = async () => {
+    if (message && message.trim() !== '') {
+      sendMessageInChat({ id, payload: { content: message.trim() } })
+      setMessage('')
+    }
+  }
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behavior: 'smooth' })
+    }
+  }, [chats[id]])
+
   return (
-    <div className="chat-board">
-
+    <div className="chat-board paper">
+      <div className="messages no-scrollbar">
+        {
+          chats[id].map((chat, index) => (<Message key={index} chat={chat} />))
+        }
+        <div ref={scrollRef} />
+      </div>
+      <div className="input-field">
+        <textarea
+          className='paper-1 no-scrollbar'
+          rows={1}
+          type='text'
+          name='message'
+          placeholder='Message'
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={handleKeyPress}
+        />
+      </div>
     </div>
   )
 }
